c3e8de2693e045fbd3469835d0d2f1593a6beb2a
mkerr6872 <kerr.mlee@gmail.com>
2023-12-06 20:48:51 -0500

    file structure

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 import numpy as np
-import string
 import pickle
 import nltk
 import math
 import os
 from collections import Counter
+import praw
+import re
+
 
 class DataPocket:
     def __init__(self, name):
@@ -16,22 +18,35 @@ class DataPocket:
         self.stdDevSent = 0.0
         self.wordFreq = Counter()
         self.source = name
+        
+    def __str__(self):
+        return f'Source: {self.source}, avgWordLen: {self.avgWordLength}, avgSentLen: {self.avgSentLength}'
+        
+def logPoisson(avg, count):
+    return math.log(avg**count * math.exp(-avg) / math.factorial(count))
 
-def dataAnalysis(document):
-    pkFile = open(document + ".pickle", "ab")
+def logGaussian(mu, sigma, x):
+    return math.log((math.exp((-1/2)*(((x-mu)/sigma)**2)) * (1/(sigma*math.sqrt(2*math.pi)))))
+
+def dataAnalysis(document, Train):
+    pkFile = open("objects/" + document + ".pickle", "ab")
     test = DataPocket(document)
-    filePtr = open(document, "r")
+    if Train == True:
+        filePtr = open("data/" + document, "r", encoding="utf-8")
+    else:
+        filePtr = open("test/" + document, "r", encoding="utf-8")
     wordLength = []
     sentLength = []
     for line in filePtr:
         sentArray = nltk.sent_tokenize(line)
         for sent in sentArray:
-            sent = sent.translate(str.maketrans('', '', string.punctuation + '’“”'))
+            sent = re.sub(r"[,.;@#?!&$—–“”]+\ *", " ", sent)
+            sent = re.sub(r"['’]+\ *", "", sent)
             test.sentCount +=1
             wordArray = nltk.word_tokenize(sent)
             sentLength.append(len(wordArray))
             for word in wordArray:
-                if len(word) > 4:
+                if len(word) > 0:
                     test.wordCount += 1
                     wordLength.append(len(word))
                     if word[0].islower():
@@ -44,40 +59,55 @@ def dataAnalysis(document):
     test.avgSentLength = np.mean(sentLength)
     test.stdDevWord = np.std(wordLength)
     test.stdDevSent = np.std(sentLength)
-
-    #print(test.avgWordLength,test.stdDevWord, test.avgSentLength , test.stdDevSent )
     
     pickle.dump(test, pkFile)
-                
-def pickleTest(document):
-    pkFile = open(document + ".pickle", 'rb')
-    test = pickle.load(pkFile)
-    print(test.source)
-    print(test.avgWordLength)
-    print(test.avgSentLength)
-    print(test.stdDevWord)
-    print(test.stdDevSent)
-    print(test.wordFreq)
+    
+def combineAnalysis():
+    if os.path.exists('objects/combined.pickle'):
+        os.remove('objects/combined.pickle')
+    test = DataPocket('combined')
+    
+    pkFile = open("objects/Fox.txt.pickle", 'rb')
+    fox = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/CNBC.txt.pickle", 'rb')
+    cnbc = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/BBC.txt.pickle", 'rb')
+    bbc = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/Apple.txt.pickle", 'rb')
+    apple = pickle.load(pkFile)
+    pkFile.close()
+    
+    pkFile = open('objects/combined.pickle', 'ab')
+    test.wordFreq = fox.wordFreq + cnbc.wordFreq + bbc.wordFreq + apple.wordFreq
+    test.wordCount = fox.wordCount + cnbc.wordCount + bbc.wordCount + apple.wordCount
+    
+    pickle.dump(test, pkFile)
     
 def priorMath():
-    if os.path.exists('BBC.txt.pickle'):
-        os.remove('BBC.txt.pickle')
-    if os.path.exists('Fox.txt.pickle'):
-        os.remove('Fox.txt.pickle')
+    if os.path.exists('objects/BBC.txt.pickle'):
+        os.remove('objects/BBC.txt.pickle')
+    if os.path.exists('objects/Fox.txt.pickle'):
+        os.remove('objects/Fox.txt.pickle')
     if os.path.exists('CNBC.txt.pickle'):
-        os.remove('CNBC.txt.pickle')
+        os.remove('objects/CNBC.txt.pickle')
     if os.path.exists('Apple.txt.pickle'):
-        os.remove('Apple.txt.pickle')
+        os.remove('objects/Apple.txt.pickle')
         
-    dataAnalysis('Fox.txt')
-    dataAnalysis('CNBC.txt')
-    dataAnalysis('BBC.txt')
-    dataAnalysis('Apple.txt')
+    dataAnalysis('Fox.txt', True)
+    dataAnalysis('CNBC.txt', True)
+    dataAnalysis('BBC.txt', True)
+    dataAnalysis('Apple.txt', True)
     
 def likelihoodCalc(sample, data,tFreq, tCount):
     lkProb = 0
-    lkProb = lkProb + logGaussian(data.avgWordLength, data.stdDevWord, sample.avgWordLength)
-    lkProb = lkProb + logGaussian(data.avgSentLength, data.stdDevSent, sample.avgSentLength)
+    #lkProb = lkProb + logGaussian(data.avgWordLength, data.stdDevWord, sample.avgWordLength)
+    #lkProb = lkProb + logGaussian(data.avgSentLength, data.stdDevSent, sample.avgSentLength)
     
     for word, freq in sample.wordFreq.items():
         if word in data.wordFreq.keys():
@@ -86,56 +116,229 @@ def likelihoodCalc(sample, data,tFreq, tCount):
             lkProb += logPoisson(tFreq[word]*sample.wordCount/tCount, sample.wordFreq[word])
     return lkProb
     
-def sourceGuess(sample):
+def sourceNewsGuess(sample):
     if os.path.exists(sample+'.pickle'):
         os.remove(sample+'.pickle')
     
-    dataAnalysis(sample)
+    dataAnalysis(sample, False)
     postProb = {}
-    pkFile = open(sample +'.pickle', 'rb')
+    pkFile = open("objects/" + sample +'.pickle', 'rb')
     sampleObject = pickle.load(pkFile)
     pkFile.close()
     
-    pkFile = open("Fox.txt.pickle", 'rb')
+    pkFile = open("objects/Fox.txt.pickle", 'rb')
     fox = pickle.load(pkFile)
     pkFile.close()
 
-    pkFile = open("CNBC.txt.pickle", 'rb')
+    pkFile = open("objects/CNBC.txt.pickle", 'rb')
     cnbc = pickle.load(pkFile)
     pkFile.close()
 
-    pkFile = open("BBC.txt.pickle", 'rb')
+    pkFile = open("objects/BBC.txt.pickle", 'rb')
     bbc = pickle.load(pkFile)
     pkFile.close()
 
-    pkFile = open("Apple.txt.pickle", 'rb')
+    pkFile = open("objects/Apple.txt.pickle", 'rb')
     apple = pickle.load(pkFile)
     pkFile.close()
     
-    totalFreq = fox.wordFreq + cnbc.wordFreq + bbc.wordFreq + apple.wordFreq
-    totalWords = fox.wordCount + cnbc.wordCount + bbc.wordCount + apple.wordCount
+    pkFile = open("objects/combined.pickle", 'rb')
+    combined = pickle.load(pkFile)
+    pkFile.close()
+    
+    totalFreq = combined.wordFreq
+    totalWords = combined.wordCount
 
     postProb['Fox'] = likelihoodCalc(sampleObject, fox, totalFreq, totalWords)
     postProb['CNBC']= likelihoodCalc(sampleObject, cnbc, totalFreq, totalWords)
     postProb['BBC'] = likelihoodCalc(sampleObject, bbc, totalFreq, totalWords)
-    postProb['Apple'] = likelihoodCalc(sampleObject, apple, totalFreq, totalWords)
-    
-    print(postProb)
+    postProb['Al Jazeera'] = likelihoodCalc(sampleObject, apple, totalFreq, totalWords)
     
     v = list(postProb.values())
     k = list(postProb.keys())
     
     print("Classified as :", k[v.index(max(v))])
+    print("Log Likelihood:")
+    print(postProb)
        
-def logPoisson(avg, count):
-    return math.log(avg**count * math.exp(-avg) / math.factorial(count))
+       
+def verificationCheck(verFiles):
+    for case, tag in verFiles.items():
+        print('---------------------------')
+        print("Source: " + tag)
+        sourceNewsGuess(case)
+        os.remove("objects/" + case+'.pickle')
 
-def logGaussian(mu, sigma, x):
-    return math.log((math.exp((-1/2)*(((x-mu)/sigma)**2)) * (1/(sigma*math.sqrt(2*math.pi)))))
+def commentScraper(sRed):
+    filept = open(sRed+".txt", "w", encoding="utf-8")
+    reddit = praw.Reddit(client_id='VtNzPEQvBnXPraApATXWFg', client_secret='ZPIRrZ5XWNbgWgFvkKPhsVdfVGpTfw', user_agent='Stochastic Project')
+    hot_posts = reddit.subreddit(sRed).top(limit=50)
+    for post in hot_posts:
+        post.comments.replace_more(limit=0)
+        for top_level_comment in post.comments:
+            filept.write(top_level_comment.body)
+            
+def redditPrep():
+    commentScraper('cars')
+    commentScraper('politics')
+    commentScraper('army')
+    commentScraper('gaming')
+    
+def redditAnalysis():
+    if os.path.exists('objects/cars.txt.pickle'):
+        os.remove('objects/cars.txt.pickle')
+    if os.path.exists('objects/politics.txt.pickle'):
+        os.remove('objects/politics.txt.pickle')
+    if os.path.exists('objects/army.txt.pickle'):
+        os.remove('objects/army.txt.pickle')
+    if os.path.exists('objects/gaming.txt.pickle'):
+        os.remove('objects/gaming.txt.pickle')
+        
+    dataAnalysis('cars.txt', True)
+    dataAnalysis('politics.txt', True)
+    dataAnalysis('army.txt', True)
+    dataAnalysis('gaming.txt', True)
+    
+    combineRedditAnalysis()
+    
+def combineRedditAnalysis():
+    if os.path.exists('objects/combinedReddit.pickle'):
+        os.remove('objects/combinedReddit.pickle')
+    test = DataPocket('combined')
+    
+    pkFile = open("objects/cars.txt.pickle", 'rb')
+    fox = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/politics.txt.pickle", 'rb')
+    cnbc = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/army.txt.pickle", 'rb')
+    bbc = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/gaming.txt.pickle", 'rb')
+    apple = pickle.load(pkFile)
+    pkFile.close()
+    
+    pkFile = open('objects/combinedReddit.pickle', 'ab')
+    test.wordFreq = fox.wordFreq + cnbc.wordFreq + bbc.wordFreq + apple.wordFreq
+    test.wordCount = fox.wordCount + cnbc.wordCount + bbc.wordCount + apple.wordCount
+    
+    pickle.dump(test, pkFile)
+    
+def sourceRedditGuess(sample):
+    if os.path.exists(sample+'.pickle'):
+        os.remove(sample+'.pickle')
+    
+    dataAnalysis(sample, False)
+    postProb = {}
+    pkFile = open("objects/" + sample +'.pickle', 'rb')
+    sampleObject = pickle.load(pkFile)
+    pkFile.close()
+    
+    pkFile = open("objects/cars.txt.pickle", 'rb')
+    cars = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/politics.txt.pickle", 'rb')
+    politics = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/army.txt.pickle", 'rb')
+    army = pickle.load(pkFile)
+    pkFile.close()
+
+    pkFile = open("objects/gaming.txt.pickle", 'rb')
+    gaming = pickle.load(pkFile)
+    pkFile.close()
+    
+    pkFile = open("objects/combinedReddit.pickle", 'rb')
+    combined = pickle.load(pkFile)
+    pkFile.close()
+    
+    totalFreq = combined.wordFreq
+    totalWords = combined.wordCount
+
+    postProb['cars'] = likelihoodCalc(sampleObject, cars, totalFreq, totalWords)
+    postProb['politics']= likelihoodCalc(sampleObject, politics, totalFreq, totalWords)
+    postProb['army'] = likelihoodCalc(sampleObject, army, totalFreq, totalWords)
+    postProb['gaming'] = likelihoodCalc(sampleObject, gaming, totalFreq, totalWords)
+    
+    v = list(postProb.values())
+    k = list(postProb.keys())
+    
+    print("Classified as :", k[v.index(max(v))])
+    print("Log Likelihood:")
+    print(postProb)
+    
+def verificationRedditCheck(verFiles):
+    for case, tag in verFiles.items():
+        print('---------------------------')
+        print("Source: " + tag)
+        sourceRedditGuess(case)
+        os.remove("objects/"+ case+'.pickle')
+
+def newspaper():
+    priorMath()
+    combineAnalysis()
+    
+    #verFiles = {'ver1.txt' : 'Fox', 'ver2.txt': 'CNBC', 'ver3.txt': "BBC", 'ver4.txt': 'Al Jazeera', 'ver5.txt': 'Fox'}
+    testFiles = {'test1.txt': 'Fox','test2.txt': 'Fox', 'test3.txt': 'Fox', 'test4.txt': 'CNBC', 'test5.txt': 'CNBC', 'test6.txt': 'CNBC'
+                 , 'test7.txt': 'BBC', 'test8.txt': 'BBC', 'test9.txt': 'BBC', 'test10.txt': 'Al Jazeera', 'test11.txt': 'Al Jazeera'
+                 , 'test12.txt': 'Al Jazeera'}
+    
+    #print('----------------Verification----------------')
+    #verificationCheck(verFiles)
+    print('----------------Testing----------------')
+    verificationCheck(testFiles)
+    
+def reddit():
+    #redditPrep()
+    redditAnalysis()
+    
+    testFiles = {'red1.txt': 'cars','red2.txt': 'cars', 'red3.txt': 'cars', 'red4.txt': 'cars'
+                 , 'red21.txt': 'cars', 'red22.txt': 'cars', 'red23.txt': 'cars', 'red24.txt': 'cars'
+                 , 'red5.txt': 'politics', 'red6.txt': 'politics', 'red7.txt': 'politics', 'red8.txt': 'politics'
+                 , 'red17.txt': 'politics', 'red18.txt': 'politics', 'red19.txt': 'politics', 'red20.txt': 'politics'
+                 , 'red9.txt': 'army', 'red10.txt': 'army', 'red11.txt': 'army', 'red12.txt': 'army'
+                 , 'red29.txt': 'army', 'red30.txt': 'army', 'red31.txt': 'army', 'red32.txt': 'army'
+                 , 'red13.txt': 'gaming', 'red14.txt': 'gaming', 'red15.txt': 'gaming', 'red16.txt': 'gaming'
+                 , 'red25.txt': 'gaming', 'red26.txt': 'gaming', 'red27.txt': 'gaming', 'red28.txt': 'gaming'}
+    
+    print('----------------Testing----------------')
+    verificationRedditCheck(testFiles)
+    
+def displayStats(name):
+        fp = open(name, 'rb')
+        data = pickle.load(fp)
+        print("Source:", data.source)
+        print("wordCount:",data.wordCount)
+        print("sentCount:",data.sentCount)
+        print("avgWordLength:",data.avgWordLength)
+        print("stdDevWord:",data.stdDevWord)
+        print("avgSentLength:",data.avgSentLength)
+        print("stdDevSent:",data.stdDevSent)
+        print('Unique words:', len(data.wordFreq))
+        
+def displayAll():
+    buckets = ['Fox.txt.pickle', 'CNBC.txt.pickle','BBC.txt.pickle','Apple.txt.pickle'
+               ,'cars.txt.pickle','politics.txt.pickle','army.txt.pickle','gaming.txt.pickle', 'combined.pickle', 'combinedReddit.pickle']
+    for thing in buckets:
+        displayStats(thing)
+
     
 if __name__ == '__main__':
-    #priorMath()
-    sourceGuess('test.txt')
+    newspaper()
+    reddit()
+    
+    #displayAll()
+
+
+    
+    
+    
 
 
     
